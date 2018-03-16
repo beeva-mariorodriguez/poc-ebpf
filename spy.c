1c5a4d2f2912b1ae19c7382e2958e85f9f3be200
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-03-16 12:54:50 +0100

    documentation

--- spy.c
+++ spy.c
@@ -1,3 +1,7 @@
+/*
+ * spy.c
+ */
+
 // https://github.com/iovisor/bcc/blob/master/docs/reference_guide.md#7-bpf_get_current_task
 // #defines needed for linux 4.13
 #define randomized_struct_fields_start  struct {
@@ -7,38 +11,88 @@
 #include <uapi/linux/limits.h> 
 #include <linux/sched.h>
 
+// Creates a buffer for pushing out event data to user space
+BPF_PERF_OUTPUT(events);
+
+// the struct we will be pushing to user space via events
+enum event_type {
+    OPEN,
+    EXEC,
+};
+
 struct event_t {
+    enum event_type type;
     char fname[NAME_MAX];
     u32 pid;
     u32 ppid;
 };
 
-BPF_PERF_OUTPUT(events);
 
-int trace_open(struct pt_regs *ctx, int dfd, const char __user *filename)
+/*
+ * trace open files
+ * man 2 open:
+ *  int open(const char *pathname, int flags);
+ *  int open(const char *pathname, int flags, mode_t mode);
+ *  int creat(const char *pathname, mode_t mode);
+ *  int openat(int dirfd, const char *pathname, int flags);
+ *  int openat(int dirfd, const char *pathname, int flags, mode_t mode);
+ * 5 different syscalls ($LINUX_SOURCES/fs/open.c)
+ * all call do_sys_open()!
+ * ./include/linux/fs.h
+ *  extern long do_sys_open(int dfd, const char __user *filename, int flags,
+ *                          umode_t mode);
+ * trace common point: do_sys_open()
+*/
+int kprobe__do_sys_open(struct pt_regs *ctx, int dfd, const char __user *filename)
 {
     u32 pid, ppid;
     struct task_struct *task = NULL;
     struct event_t event = {};
 
-
-    pid = bpf_get_current_pid_tgid();
+    // get in kernel process identificator (thread group id + process id)
+    // userspace PID == kernel TGID
+    // userspace TID == kernel PID
+    // linux is hard
+    // cast 64 bit tgid+pid to u32 variable to get the tgid
+    pid = (u32) bpf_get_current_pid_tgid();
+    // get task_struct
+    // lots of info (including a link to the parent's task_struct
+    // defined in $LINUX_SOURCES/include/linux/sched.h
     task = (struct task_struct *)bpf_get_current_task();
     if (!task){
         return 0;
     }
 
+    // ignore kernel tasks
     if (task->flags & PF_KTHREAD)
         return 0;
 
+    // task->real_parent is the task_struct for the parent process
     ppid = task->real_parent->pid;
  
+    // placeholder for a user defined filter
+    // spoiler: it's the target's pid
     FILTER
 
+    // populate event
+    event.type =  OPEN;
     event.pid = pid;
     event.ppid = ppid;
+    // *filename is a pointer to a string containing the filename
+    // we can't (directly) read kernel memory
+    // bpf_probe_read() macro will (safely) copy it for us
     bpf_probe_read(&event.fname, sizeof(event.fname), (void *)filename);
+    // event ready, submit
     events.perf_submit(ctx, &event, sizeof(event));
+
     return 0;
 }
 
+// man 2 execve
+int trace_exec(struct pt_regs *ctx,
+               const char __user *filename,
+               const char __user *const __user *__argv,
+               const char __user *const __user *__envp)
+{
+    return 0;
+}
--- spy.py
+++ spy.py
@@ -3,20 +3,29 @@ from bcc import BPF
 import ctypes as ct
 import argparse
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("pid")
 args = parser.parse_args() 
 
+# read ebpf C code to bpf_text
 with open('./spy.c', 'r') as f:
   bpf_text = f.read()
 
+# replace the placeholder (FILTER) with a check
 bpf_text = bpf_text.replace('FILTER', 'if (pid != %s && ppid != %s) { return 0; }' % (args.pid, args.pid))
 b = BPF(text=bpf_text)
-b.attach_kprobe(event="do_sys_open", fn_name="trace_open")
 
 NAME_MAX = 255 # linux/limits.h
+
+# event definition, should be compatible with enum event_type and struct event_t from spy.c
+class EventType(object):
+    OPEN = 0
+    EXEC = 1
+
 class Event(ct.Structure):
-    _fields_ = [("fname", ct.c_char * NAME_MAX),
+    _fields_ = [("type", ct.c_int),
+                ("fname", ct.c_char * NAME_MAX),
                 ("pid", ct.c_uint),
                 ("ppid", ct.c_uint)]
 
@@ -24,8 +33,13 @@ print "tracing %s" % args.pid
 
 def print_event(cpu,data,size):
     event = ct.cast(data, ct.POINTER(Event)).contents
-    print "fname: %s, pid: %d, ppid: %d" % (event.fname, event.pid, event.ppid)
+    if event.type == EventType.OPEN:
+        print "%d->%d > open(%s)" % (event.ppid, event.pid, event.fname)
+    if event.type == EventType.EXEC:
+        print "%d->%d > exec()" % (event.ppid, event.pid)
+
 
 b["events"].open_perf_buffer(print_event)
+
 while True:
     b.kprobe_poll()
